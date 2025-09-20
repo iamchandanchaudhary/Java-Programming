caa2212553071a8fe3cdfd618b3b233998196213
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-09-20 23:44:35 +0530

    Improvement 57

---
+++ Level-11 (2D Arrays)/Sum of Array.java
@@ -0,0 +1,29 @@
+class FirstArray {
+    public static void main(String args[]){
+        System.out.println("2D Arrays:- \n");
+
+        int numbers[][] = {{2, 3, 4, 5}, {6, 7, 8, 9}, {7, 4, 2, 3}};
+
+        int row = numbers.length;
+        int col = numbers[0].length;
+
+        int sum = 0;
+
+        System.out.println("Your 2D Array: ");
+        //output
+        for(int i = 0; i < row; i++) { //rows
+            for(int j = 0; j < col; j++) { //cols
+                System.out.print(numbers[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        for(int i = 0; i < row; i++) {
+            for(int j = 0; j < col; j++) {
+                sum += numbers[1][j];
+            }
+        }
+
+        System.out.println("Sum: " + sum);
+    }
+}
